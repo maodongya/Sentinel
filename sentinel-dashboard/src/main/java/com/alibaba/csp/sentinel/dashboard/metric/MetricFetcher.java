24d9ab19819032a33086e72fbafb98906e992110
maodongya <mdy2010life@163.com>
2022-04-14 16:09:06 +0800

    mysql save.

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/metric/MetricFetcher.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/metric/MetricFetcher.java
@@ -59,6 +59,7 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -79,7 +80,7 @@ public class MetricFetcher {
     private final long intervalSecond = 1;
 
     private Map<String, AtomicLong> appLastFetchTime = new ConcurrentHashMap<>();
-
+    @Qualifier("jpaMetricsRepository")
     @Autowired
     private MetricsRepository<MetricEntity> metricStore;
     @Autowired
